5860c5add8c868414e3c8ee35dce954a3c0f8383
Adam Hu <adamhu@Adams-MacBook-Air-2.local>
2023-07-14 20:07:54 -0400

    Added editting my details

--- social_network_ui.py
+++ social_network_ui.py
@@ -1,6 +1,9 @@
 # You can implement user interface functions here.
+from social_network_classes import SocialNetwork
+import os
 
 def mainMenu():
+    os.system('clear')
     print("")
     print("1. Create a new account")
     print("2. Manage my account")
@@ -9,6 +12,7 @@ def mainMenu():
     return input("Please Choose a number: ")
 
 def manageAccountMenu():
+    os.system('clear')
     print("")
     print("1. Edit my details")
     print("2. Add a friend")
@@ -16,3 +20,14 @@ def manageAccountMenu():
     print("4. View all my messages")
     print("5. <- Go back ")
     return input("Please Choose a number: ")
+
+def editAccountMenu():
+    os.system('clear')
+    print("")
+    print("1. Edit my name")
+    print("2. Edit my age")
+    print("3. Edit my bio")
+    print("4. View my details and bio")
+    print("5. <- Go back ")
+    return input("Please Choose a number: ")
+    
