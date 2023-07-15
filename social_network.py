c30c8e608eb8299484d0aa628466b60c916a653c
Adam Hu <adamhu@Adams-MacBook-Air-2.local>
2023-07-14 20:07:07 -0400

    Added editting my details

--- social_network.py
+++ social_network.py
@@ -1,12 +1,14 @@
 #Various import Statements can go here
 from  social_network_classes import SocialNetwork,Person
 import social_network_ui
+import os
+
 
 
 
 #Create instance of main social network object
 ai_social_network = SocialNetwork()
-
+user = Person("", "", "")
 #The line below is a python keyword to specify which 
 if __name__ == "__main__":
     print("########################################################")
@@ -24,6 +26,34 @@ if __name__ == "__main__":
             inner_menu_choice = social_network_ui.manageAccountMenu()
             #Handle inner menu here
             while True:
+                if inner_menu_choice == "1":
+                    inner_inner_choice = social_network_ui.editAccountMenu()
+                    while True:
+                        if inner_inner_choice == "1":
+                            os.system('clear')
+                            user.name = input("Input Name: ")
+                            os.system('clear')
+                            inner_inner_choice = social_network_ui.editAccountMenu()
+                        if inner_inner_choice == "2":
+                            os.system('clear')
+                            user.age = input("Input Age: ")
+                            os.system('clear')
+                            inner_inner_choice = social_network_ui.editAccountMenu()
+                        if inner_inner_choice == "3":
+                            os.system('clear')
+                            user.bio = input("Input Bio: ")
+                            os.system('clear')
+                            inner_inner_choice = social_network_ui.editAccountMenu()   
+                        if inner_inner_choice == "4":
+                            os.system('clear')
+                            print("Name:", user.name)
+                            print("Age:", user.age)
+                            print("Bio:", user.bio)
+                            input("Press enter to go back\n")
+                            os.system('clear')
+                            inner_inner_choice = social_network_ui.editAccountMenu()
+                        if inner_inner_choice == "5":
+                            break
                 if inner_menu_choice == "5":
                     break
                 else:
