77d222e5b614df650b6cfbf3bbdc5717ef8c9cf6
Adam Hu <adamhu@Adams-MacBook-Air-2.local>
2023-07-14 21:09:24 -0400

    added friends stuff and separated User and Person

--- social_network_classes.py
+++ social_network_classes.py
@@ -1,4 +1,5 @@
 # A class to hold general system wide social media data and functions. Eg Data objects of all people, Eg functions: Save social media to disk
+import os
 class SocialNetwork:
     def __init__(self):
         self.list_of_people = [] # this instance variable is initialized to an empty list when social network is created, 
@@ -10,23 +11,22 @@ class SocialNetwork:
         # hint: look up how to use python's inbuil json module to turn objects to json
         # you can write this json unto a file on disk
         pass
+    def add_account(self, person_object):
+        self.list_of_people.append(person_object)
 
     ## For more challenge try this
     def reload_social_media(self):
+        pass
         # function to load saved social media from file on disk 
         # hint: load a the json file from disk and look up how to recreate the list of people objects.
-        pass
+   # def create_account(self, person_object):
+    #    self.list_of_people.append(person_object)
 
-    def  create_account(self):
-        #implement function that creates account here
-        print("Creating ...")
-        pass
-
-
-class Person:
-    def __init__(self, name, age):
-        self.id = name
-        self.year = age
+class User:
+    def __init__(self, name, age, bio):
+        self.name = name
+        self.age = age
+        self.bio = bio
         self.friendlist = []
 
     def add_friend(self, person_object):
@@ -36,3 +36,9 @@ class Person:
     def send_message(self):
         #implement sending message to friend here
         pass
+
+class Person:
+    def __init__(self, name, age, bio):
+        self.name = name
+        self.age = age
+        self.bio = bio
