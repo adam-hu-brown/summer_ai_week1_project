c1471b2a23a1dcc7572c4c5ea2a0ea6d8ed31ef9
Adam Hu <adamhu@adams-air-2.devices.brown.edu>
2023-07-16 00:52:59 -0400

    added saving

--- social_network_classes.py
+++ social_network_classes.py
@@ -1,43 +1,54 @@
 import os
+import json
 class SocialNetwork:
     def __init__(self):
         self.list_of_people = [] 
-        
-    ## For more challenge try this
-    def save_social_media(self):
-        # function to save social media to a file on disk 
-        # hint: look up how to use python's inbuil json module to turn objects to json
-        # you can write this json unto a file on disk
-        pass
 
     def createAccount(self, person_object):
         self.list_of_people.append(person_object)
 
-    ## For more challenge try this
-    def reload_social_media(self):
-        pass
-        # function to load saved social media from file on disk 
-        # hint: load a the json file from disk and look up how to recreate the list of people objects.
+    def save_social_media(self, user_object):
+        with open('list_of_people.json', 'w') as f:
+            data = json.dumps([person.__dict__ for person in self.list_of_people], indent=2, sort_keys=True)
+            f.write(data)
+        with open('messagelist.json', 'w') as f:
+            data = json.dumps([message for message in user_object.messagelist], indent=2, sort_keys=True)
+            f.write(data)
+        with open('user_info.json', 'w') as f:
+            data = json.dumps([user_object.__dict__], indent=2, sort_keys=True)
+            f.write(data)
+
+    def reload_social_media(self, user_object):
+        with open('list_of_people.json', 'r') as f:
+            data = f.read()
+            data = json.loads(data)
+            for i in data:
+                self.createAccount(Person(str(i['name']), str(i['age']), str(i['bio']), str(i['isFriend'])))
+        with open('messagelist.json', 'r') as f:
+            data = f.read()
+            data = json.loads(data)
+            for message in data:
+                user_object.messagelist.append(message)
+ 
+
+        
 
 class User:
     def __init__(self, name, age, bio):
         self.name = name
         self.age = age
         self.bio = bio
-        self.friendlist = []
         self.messagelist = []
 
     def block(self, social_network_object, person_object):
         social_network_object.list_of_people.remove(person_object)
 
-    def addFriend(self, person_object):
-        self.friendlist.append(person_object)
-
     def recieveMessage(self, message, person_object):
         self.messagelist.append("From " + person_object.name + ":\n      " + message)
 
 class Person:
-    def __init__(self, name, age, bio):
+    def __init__(self, name, age, bio, isFriend):
         self.name = name
         self.age = age
         self.bio = bio
+        self.isFriend = isFriend
